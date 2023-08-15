8194e2c2559b29227d96dcf484420898c085fe70
Saurav-Singh-Rauthan <sauravsinghrauthan@gmail.com>
2023-08-15 23:05:07 +0530

    cleanup

--- src/constants/colors.ts
+++
@@ -1,8 +0,0 @@
-export const Colors = {
-  blackPrimary: '#0b0b0d',
-  blackSecondary: '#181619',
-  goldPrimary: '#a76b09',
-  goldSecondary: '#dda74f',
-  bluePrimary: '#272a31',
-  blueSecondary: '#181b22'
-}
