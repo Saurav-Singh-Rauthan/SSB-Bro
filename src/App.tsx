4c986e577616b9be786d518b4f524dbfd8f959a8
Saurav-Singh-Rauthan <sauravsinghrauthan@gmail.com>
2023-08-15 23:05:50 +0530

    ui/framer components

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,29 @@
-import "./App.css";
-import { Route, Routes } from "react-router-dom";
+import "./App.scss";
+import { Route, Routes, useLocation } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
 
+import WrapperWithNavbar from "./components/containers/WrapperWithNavbar/WrapperWithNavbar";
 import Home from "./pages/home/home";
-import NotFound from './pages/404/404'
+import NotFound from "./pages/404/404";
+import Explore from "./pages/explore/explore";
+import About from "./pages/about/about";
+import Courses from "./pages/courses/courses";
 
 function App() {
+  const location = useLocation();
+
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+    <AnimatePresence mode="sync">
+      <Routes location={location} key={location.key}>
+        <Route element={<WrapperWithNavbar />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/courses" element={<Courses />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </AnimatePresence>
   );
 }
 
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,11 +1,32 @@
-import Styles from "./home.module.css";
+import Styles from "./home.module.scss";
+import { motion } from "framer-motion";
+
+const containerVariants = {
+  initial: {
+    x: "-100vw",
+    transition: {
+      delay: 0.5,
+      ease: "easeInOut",
+    },
+  },
+  animate: {
+    x: 0,
+    transition: {
+      delay: 0.5,
+      ease: "easeInOut",
+    },
+  },
+};
 
 const Home = () => {
   return (
-    <div className={Styles.mainContainer}>
-      <h1>SSB Bro Main Website</h1>
-      <p className="read-the-docs">this is a placeholder website</p>
-    </div>
+    <motion.div
+      className={Styles.mainContainer}
+      variants={containerVariants}
+      initial="initial"
+      animate="animate"
+      exit="initial"
+    ></motion.div>
   );
 };
 
