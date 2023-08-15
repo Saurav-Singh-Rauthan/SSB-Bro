88243fe62cac16110834252152b9436b5c489756
Saurav-Singh-Rauthan <sauravsinghrauthan@gmail.com>
2023-08-15 23:02:43 +0530

    ui/pages init

---
+++ src/pages/courses/courses.tsx
@@ -0,0 +1,33 @@
+import { motion } from "framer-motion";
+
+const containerVariants = {
+  initial: {
+    x: "-100vw",
+    transition: {
+      delay: 0.5,
+      ease: "easeInOut",
+    },
+  },
+  animate: {
+    x: 0,
+    transition: {
+      delay: 0.5,
+      ease: "easeInOut",
+    },
+  },
+};
+
+const Courses = () => {
+  return (
+    <motion.div
+      variants={containerVariants}
+      initial="initial"
+      animate="animate"
+      exit="initial"
+    >
+      <p>Courses Section</p>
+    </motion.div>
+  );
+};
+
+export default Courses;
